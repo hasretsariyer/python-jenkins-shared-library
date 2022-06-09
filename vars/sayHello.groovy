cb0f14fae4d5d1dce42f3ff6050d8c8b5bbe1019
hasretsariyer <hasretsariyer@gmail.com>
2022-06-09 15:42:35 +0200

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 def call() {
   final pythonContent = libraryResource('sharedlibrary/myFile.py')
-  // There are definitely better ways to do this without having to write to the consumer's workspace
+  
   writeFile(file: 'my_file.py', text: pythonContent)
   sh 'chmod +x my_file.py && python ./my_file.py'
 }

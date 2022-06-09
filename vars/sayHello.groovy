1b0e3102bb5aa4e9ef68d626be2d8ae28c7aede1
hasretsariyer <hasretsariyer@gmail.com>
2022-06-09 14:33:49 +0200

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,4 @@
-def sayHello() {
+def call() {
   final pythonContent = libraryResource('sharedlibrary/myFile.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'my_file.py', text: pythonContent)

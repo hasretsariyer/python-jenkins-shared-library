457b9827cb72197042be961b5bf8c2aa0689c917
hasretsariyer <hasretsariyer@gmail.com>
2022-06-10 11:43:45 +0200

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,8 @@
 def call() {
   final pythonContent = libraryResource('sharedlibrary/myFile.py')
   
-  sh 'chmod +x sharedlibrary/myFile.py && python3 sharedlibrary/myFile.py'
+  sh 'chmod +x myFile2.py && python3 myFile2.py'
   
   writeFile(file: 'my_file.py', text: pythonContent)
-  sh 'chmod +x my_file.py && python3 ./my_file.py'
+  sh 'chmod +x my_file.py && python3 my_file.py'
 }

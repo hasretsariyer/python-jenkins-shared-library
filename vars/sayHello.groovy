79d0317721b2d884daed3892300cb4a920151196
hasretsariyer <hasretsariyer@gmail.com>
2022-06-10 11:29:39 +0200

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,5 +4,5 @@ def call() {
   // sh 'chmod +x ./sharedlibrary/myFile.py && python sharedlibrary/myFile.py'
   
   writeFile(file: 'my_file.py', text: pythonContent)
-  sh 'chmod +x my_file.py && python ./my_file.py'
+  sh 'chmod +x my_file.py && python3 ./my_file.py'
 }
